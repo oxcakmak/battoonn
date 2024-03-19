c61265333197acf5b5264eea8781656824fc1dac
Osman Cakmak <oxcakmak@hotmail.com>
2024-03-19 04:48:55 +0300

    v1.1.1
    Command channel check

--- events/interactions/interactionCreate.js
+++ events/interactions/interactionCreate.js
@@ -1,24 +1,41 @@
-const {
-  SlashCommandBuilder,
-  ActionRowBuilder,
-  ButtonBuilder,
-  ButtonStyle,
-  EmbedBuilder,
-} = require("discord.js");
-const { botColor, botWebsite } = require("../../config.json");
+const { Configs } = require("../../database/schemas/config");
+const { _ } = require("../../utils/localization");
 
 module.exports = {
   name: "interactionCreate",
-
-  execute(interaction, client) {
+  async execute(interaction, client) {
     const { customId } = interaction;
+    const serverId = interaction.guild.id;
+    const server = await Configs.findOne({ server: serverId });
 
     if (interaction.isChatInputCommand()) {
       const command = client.commands.get(interaction.commandName);
-      if (!command) {
-        interaction.reply({ content: _("outdated_command") });
+      const responseChannel = await client.channels.cache.get(
+        server.responseChannel
+      );
+
+      if (!command)
+        return await interaction.reply({ content: _("outdated_command") });
+
+      if (
+        server.commandChannel &&
+        interaction.channelId !== server.commandChannel
+      ) {
+        if (responseChannel)
+          return await responseChannel.send({
+            content: _("commands_only_run_channel_variable_mention_user", {
+              channel: `<#${server.commandChannel}>`,
+              user: `<@${interaction.user.id}>`,
+            }),
+          });
+
+        return await interaction.reply({
+          content: _("commands_only_run_channel"),
+          ephemeral: true,
+        });
       }
-      command.execute(interaction, client);
+    } else {
+      await command.execute(interaction, client);
     }
   },
 };
--- events/message/messageCreate.js
+++ events/message/messageCreate.js
@@ -1,34 +1,45 @@
 const {
-    EmbedBuilder,
-    ButtonStyle,
-    ActionRowBuilder,
-    ButtonBuilder,
+  EmbedBuilder,
+  ButtonStyle,
+  ActionRowBuilder,
+  ButtonBuilder,
 } = require("discord.js");
-const { botId, botName, botAvatar, botWebsite, botColor } = require("../../config.json");
+const {
+  botId,
+  botName,
+  botAvatar,
+  botWebsite,
+  botColor,
+} = require("../../config.json");
+
+const { Configs } = require("../../database/schemas/config");
+const { _ } = require("../../utils/localization");
 
 module.exports = {
-    name: "messageCreate",
+  name: "messageCreate",
+  async execute(message) {
+    const serverId = message.channel.id;
+    const server = await Configs.findOne({ server: serverId });
 
-    execute(message) {
-        if (!message.mentions.users.first()) return;
-        if (message.mentions.users.first().id == `${botId}`) {
-            const embed = new EmbedBuilder().setColor(botColor).setAuthor({
-                name: `${botName}`,
-                iconURL: `${botAvatar}`,
-                url: `${botWebsite}`,
-            }).setDescription(`
+    if (!message.mentions.users.first()) return;
+    if (message.mentions.users.first().id == `${botId}`) {
+      const embed = new EmbedBuilder().setColor(botColor).setAuthor({
+        name: `${botName}`,
+        iconURL: `${botAvatar}`,
+        url: `${botWebsite}`,
+      }).setDescription(`
                     Thanks for using me 🤍 Have a nice day
                     /help | show all commands
                 `);
 
-            const row = new ActionRowBuilder().addComponents(
-                new ButtonBuilder()
-                    .setLabel(`Up vote`)
-                    .setStyle(ButtonStyle.Link)
-                    .setEmoji("🤍")
-                    .setURL(`${botWebsite}`)
-            );
-            message.reply({ embeds: [embed], components: [row] });
-        }
-    },
+      const row = new ActionRowBuilder().addComponents(
+        new ButtonBuilder()
+          .setLabel(`Up vote`)
+          .setStyle(ButtonStyle.Link)
+          .setEmoji("🤍")
+          .setURL(`${botWebsite}`)
+      );
+      message.reply({ embeds: [embed], components: [row] });
+    }
+  },
 };
