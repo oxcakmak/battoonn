56974304f2a8e78cfb98bfccd9a9ffebb9826671
Osman Cakmak <oxcakmak@hotmail.com>
2024-03-22 21:24:05 +0300

    v1.1.6
    join and leave module completed.

--- events/explorer/guildMemberRemove.js
+++ events/explorer/guildMemberRemove.js
@@ -1,22 +1,19 @@
 const { Explorers } = require("../../database/schemas");
 
 module.exports = {
-  name: "guildMemberAdd",
+  name: "guildMemberRemove",
   async execute(member) {
     // Use the member object directly
     const guild = member.guild; // Access guild from member for clarity
     const serverId = guild.id;
     const explorer = await Explorers.findOne({ server: serverId });
-    const { moduleEnabled, givenRole, notifyChannel, joinMessage } = explorer; // Destructuring
+    const { moduleEnabled, notifyChannel, leaveMessage } = explorer; // Destructuring
 
     if (explorer && moduleEnabled) {
-      const role = await guild.roles.cache.get(givenRole);
       const channel = await guild.channels.cache.get(notifyChannel);
 
-      if (givenRole && role) await member.roles.add(givenRole);
-
       if (channel && notifyChannel)
-        await channel.send(joinMessage.replace("%user%", `<@${member.id}>`));
+        await channel.send(leaveMessage.replace("%user%", `<@${member.id}>`));
     }
   },
 };
