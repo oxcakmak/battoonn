da97bf82d739f0c12c67a60e3c7e6a90ee001e6d
Osman Cakmak <oxcakmak@hotmail.com>
2024-03-23 22:43:00 +0300

    v1.1.7
    qr code added.

---
+++ commands/general/qr.js
@@ -0,0 +1,39 @@
+const { MessageAttachment, SlashCommandBuilder } = require("discord.js");
+const fs = require("fs");
+const qr = require("qrcode");
+const { v4: uuidv4 } = require("uuid");
+const { _ } = require("../../utils/localization");
+
+const randomIdv4 = uuidv4();
+
+async function generateQRCode(text, filePath) {}
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("qr")
+    .setDescription(_("generates_qr_code"))
+    .addStringOption((option) =>
+      option.setName("data").setDescription(_("qr_code_data")).setMaxLength(50)
+    ),
+  async execute(interaction) {
+    if (interaction.bot) return;
+
+    const data = interaction.options.getString("data");
+
+    if (!data) return;
+
+    // Generate QR code
+    const qrFileName = `qr-${interaction.user.id}-${randomIdv4}.png`;
+    const qrCodeFilePath = `qr/${qrFileName}`;
+
+    await qr.toFile(qrCodeFilePath, data);
+
+    await interaction.reply({
+      type: 4,
+      files: [qrCodeFilePath],
+    });
+
+    // Delete generated QR code file after sending
+    await fs.unlinkSync(qrCodeFilePath);
+  },
+};
