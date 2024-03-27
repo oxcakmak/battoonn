a92be03a12c95a4fa0a7e212d1c9927a18e2066b
Osman Cakmak <oxcakmak@hotmail.com>
2024-03-27 19:27:38 +0300

    v1.2.9
    twitch notify system prepared 50%

--- events/notification/twitch.js
+++ events/notification/twitch.js
@@ -1,14 +1,14 @@
 const TwitchApi = require("node-twitch").default;
 const schedule = require("node-schedule");
+const { _ } = require("../../utils/localization");
+const { twitchApiClient, twitchApiSecret } = require("../../config.json");
 
 // Replace with your Twitch Client ID and desired channel name
-const clientId = "424284ai68cd2w2p8hnjs4rnsened8";
-const clientSecret = "fsduplan40pam8nsjlhm6po91and04";
 const channelName = "osmaninalemi";
 
 const twitch = new TwitchApi({
-  client_id: clientId,
-  client_secret: clientSecret,
+  client_id: twitchApiClient,
+  client_secret: twitchApiSecret,
 });
 
 //get time local
@@ -27,30 +27,81 @@ module.exports = {
   name: "ready",
   once: true,
   async execute(client) {
+    /*
     const channelData = await twitch.getStreams({
       channel: channelName,
     });
-    const stream = channelData.data[0];
-    const userId = stream.id;
+    const stream = await channelData.data[0];
+    const userId = stream.user_id;
     const username = stream.user_login;
     const userTitle = stream.user_name;
     const category = stream.category;
     const type = stream.type;
     const title = stream.title;
+    const startedAt = stream.started_at;
     const thumbnail = stream.thumbnail_url
       .replace("{width}", 640)
       .replace("{height}", 360);
     const tags = stream.tags;
+    const viewerCount = stream.viewer_count;
+    const userData = await twitch.getUsers(userId);
+    const user = await userData.data[0];
+    const profileImage = user.profile_image_url;
+    let humanizeTags;
+    for (const tag of tags) {
+      humanizeTags += "`" + tag + "`&nbsp;";
+    }
+
+    const embed = {
+      embeds: [
+        {
+          type: "rich",
+          title: _("username_is_on_live_stream", { username: userTitle }),
+          description: "",
+          color: 0xffffff,
+          fields: [
+            {
+              name: _("stream_title"),
+              value: title,
+            },
+            {
+              name: _("category"),
+              value: category,
+              inline: true,
+            },
+            {
+              name: _("viewer"),
+              value: viewerCount,
+              inline: true,
+            },
+            {
+              name: _("tag_s"),
+              value: humanizeTags,
+            },
+          ],
+          timestamp: startedAt,
+          image: {
+            url: thumbnail,
+            proxy_url: thumbnail,
+            height: 640,
+            width: 360,
+          },
+          thumbnail: {
+            url: profileImage,
+            height: 300,
+            width: 300,
+          },
+          url: `https://twitch.tv/${username}`,
+        },
+      ],
+    };
+
     // Schedule check every 5 minutes (adjust as needed)
     const job = await schedule.scheduleJob("* * * * *", async () => {
-      const userData = await twitch.getUsers({
-        user: userId,
-      });
-      const user = userData.data[0];
-      console.log(stream);
-      if (!messageSent) {
+      if (!messageSent && type === "live") {
         messageSent = true;
       }
     });
+    */
   },
 };
