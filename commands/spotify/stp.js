a3994047d4f3aa48ebc9a1ca369e2b8dc4e9d9d7
Osman Cakmak <oxcakmak@hotmail.com>
2024-04-06 00:09:01 +0300

    v1.4.4
    update - search list fixed

--- commands/spotify/stp.js
+++ commands/spotify/stp.js
@@ -48,60 +48,55 @@ module.exports = {
         content: _("no_results_found"),
       });
 
-    let nameList;
-    let nameListJson;
-    let number = 0;
-    await tracks.forEach(async (track) => {
-      const artists = track.album.artists;
-      const artistNames = artists.map((artist) => artist.name).join(" & ");
-      const duration = track.duration_ms;
-      const humanizeDuration = await parse(duration);
-      const name = track.name;
-      const href = track.href;
-      const fullname = artistNames + " - " + name;
-      const fullTitle = fullname + `[${humanizeDuration}]`;
-      nameListJson.push({ label: fullTitle, value: number++, emoji: "" });
-    });
-    /*
-    await interaction.reply({
-      embeds: [
-        {
-          type: "rich",
-          title: `Arama sonucu`,
-          description: `asdasd`,
-          color: 0x6ed7d7,
-          fields: [
-            {
-              name: "\u200B",
-              value: `asdasdsadsaddddasdddasd`,
-            },
-          ],
-        },
-      ],
-      components: [
-        {
-          type: 1,
-          components: [
-            {
-              custom_id: `musicList`,
-              placeholder: `sadasddddd`,
-              options: [
-                {
-                  label: `Choose Music`,
-                  value: `choose`,
-                  default: true,
-                },
-                JSON.stringify(nameListJson),
-              ],
-              min_values: 1,
-              max_values: 1,
-              type: 3,
-            },
-          ],
-        },
-      ],
-    });
-    console.log(nameListJson);
-    */
+    try {
+      let trackList = [];
+      let nameList = [];
+      let numbers = 0;
+      await tracks.forEach(async (track) => {
+        const artists = track.album.artists;
+        const artistNames = artists.map((artist) => artist.name).join(" & ");
+        const duration = track.duration_ms;
+        const humanizeDuration = await parse(duration);
+        const name = track.name;
+        const href = track.href;
+        const fullname = artistNames + " - " + name;
+        const fullTitle = fullname + ` [${humanizeDuration}]`;
+        trackList.push(`${numbers}. ${fullTitle}`);
+        nameList.push({ label: `${numbers}. ${fullTitle}`, value: name });
+        numbers++;
+      });
+
+      const options = nameList.map((option) => ({
+        label: option.label,
+        value: option.value.toString(),
+      }));
+
+      return await interaction.reply({
+        embeds: [
+          {
+            type: "rich",
+            title: `Arama sonucu`,
+            description: trackList.join("\n"),
+          },
+        ],
+        components: [
+          {
+            type: 1,
+            components: [
+              {
+                custom_id: `musicList`,
+                placeholder: `sadasddddd`,
+                options,
+                min_values: 1,
+                max_values: 1,
+                type: 3,
+              },
+            ],
+          },
+        ],
+      });
+    } catch (error) {
+      console.log(error);
+    }
   },
 };
